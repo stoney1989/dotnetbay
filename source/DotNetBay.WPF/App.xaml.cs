9d322676924f133a754a349f4ab62ca2ff00dd27
stoney1989 <nico.hartma@gmail.com>
2015-03-02 10:24:41 +0100

    init new

---
+++ source/DotNetBay.WPF/App.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using DotNetBay.Core;
+using DotNetBay.Core.Execution;
+using DotNetBay.Data.FileStorage;
+using DotNetBay.Interfaces;
+using DotNetBay.Model;
+
+namespace DotNetBay.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private readonly IMainRepository mainRepository;
+        private readonly AuctionRunner auctionRunner;
+
+        public IMainRepository MainRepository
+        {
+            get { return this.mainRepository; }
+        }
+
+        public IAuctionRunner AuctionRunner
+        {
+            get { return this.auctionRunner; }
+        }
+
+        public App()
+        {
+            this.mainRepository = new FileSystemMainRepository("Main Repository");
+            this.auctionRunner = new AuctionRunner(this.mainRepository);
+            this.auctionRunner.Start();
+            this.InitDemoData();
+        }
+
+        private void InitDemoData()
+        {
+            var memberService = new SimpleMemberService(this.mainRepository);
+            var service = new AuctionService(this.mainRepository, memberService);
+            if (!service.GetAll().Any())
+            {
+
+                var me = memberService.GetCurrentMember();
+                service.Save(new Auction
+                {
+                    Title = "My First Auction",
+                    StartDateTimeUtc = DateTime.UtcNow.AddSeconds(10),
+                    EndDateTimeUtc = DateTime.UtcNow.AddDays(14),
+                    StartPrice = 72,
+                    Seller = me
+                });
+
+                service.Save(new Auction
+                {
+                    Title = "My Second Auction",
+                    StartDateTimeUtc = DateTime.UtcNow.AddSeconds(10),
+                    EndDateTimeUtc = DateTime.UtcNow.AddDays(14),
+                    StartPrice = 72,
+                    Seller = me
+                });
+
+                service.Save(new Auction
+                {
+                    Title = "My Third Auction",
+                    StartDateTimeUtc = DateTime.UtcNow.AddSeconds(10),
+                    EndDateTimeUtc = DateTime.UtcNow.AddDays(14),
+                    StartPrice = 72,
+                    Seller = me
+                });
+            }
+        }
+
+    }
+}
---
+++ source/DotNetBay.WPF/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DotNetBay.Core;
+using DotNetBay.Model;
+
+namespace DotNetBay.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private ObservableCollection<Auction> auctions;
+
+        public ObservableCollection<Auction> Auctions
+        {
+            get { return this.auctions; }
+        }
+
+        private String test = "TestString";
+
+        public String Test
+        {
+            get { return this.test; }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            var mainRepository = ((App)Application.Current).MainRepository;
+
+            var memberService = new SimpleMemberService(mainRepository);
+            var service = new AuctionService(mainRepository, memberService);
+
+            this.auctions = new ObservableCollection<Auction>(service.GetAll());
+            this.DataContext = this;
+        }
+
+        private void BuyAction(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
